de13a455e007ab7ac1be0c61dfadef968e1169fa
zhangliang <zl89456123@163.com>
2023-10-23 23:38:51 +0800

    'med'

--- model.py
+++ model.py
@@ -15,9 +15,12 @@ from glm2.configuration_chatglm import ChatGLMConfig as ChatGLM2Config
 from glm1.modeling_chatglm import ChatGLMForConditionalGeneration
 from glm1.tokenization_chatglm import ChatGLMTokenizer
 from glm1.configuration_chatglm import ChatGLMConfig
-from utils import GLMPromptDataSet, GLM2PromptDataSet
+from utils import GLMPromptDataSet, GLM2PromptDataSet, GLM2MedPromptDataSet
 
 MODE = {"glm": {"model": ChatGLMForConditionalGeneration, "tokenizer": ChatGLMTokenizer, "config": ChatGLMConfig,
                 "dataset": GLMPromptDataSet},
         "glm2": {"model": ChatGLM2ForConditionalGeneration, "tokenizer": ChatGLM2Tokenizer, "config": ChatGLM2Config,
-                 "dataset": GLM2PromptDataSet}}
+                "dataset": GLM2PromptDataSet},
+        "glm2med": {"model": ChatGLM2ForConditionalGeneration, "tokenizer": ChatGLM2Tokenizer, "config": ChatGLM2Config,
+                "dataset": GLM2MedPromptDataSet}
+        }
